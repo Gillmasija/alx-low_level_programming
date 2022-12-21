16b49201508c9ef9341040ddf399abf21855689b
Gillmasija <gillianmasija@gmail.com>
2022-12-21 01:32:58 -0800

    task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ *_strncat -  function to append char
+ *@dest: first parameter
+ *@src: second parameter
+ *@n:third parameter
+ *Return: string
+ */
+char *_strncat(char *dest, char *src, int n);
+{
+strncat(*desr, *src, n);
+return (dest);
+}
