078648d11bc68b6ffb4f8df0eb1c410d880671e2
Gillmasija <gillianmasija@gmail.com>
2023-01-03 23:43:50 -0800

    Task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,10 +6,12 @@
  */
 void print_rev(char *s)
 {
-int i = 0;
-while (s[i])
-i++;
-while (i--)
-_putchar(s[i]);
+int index;
+for (index = 0; s[index] != '\0'; index++)
+;
+for (index = index - 1; s[index] != '\0'; index--)
+{
+_putchar(s[index]);
+}
 _putchar('\n');
 }
