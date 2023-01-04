4cf6da27fbab75a1f7752a28d579ac2481d01ac9
Gillmasija <gillianmasija@gmail.com>
2023-01-04 00:51:54 -0800

    Task 12

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,12 +1,12 @@
 #include "main.h"
 #include <string.h>
 /**
- *_strncp - copies count characters of string2 to string1 
+ *_strncp - copies count characters of string2 to string1
  *@dest:first parameter
  *@src: second parameter
  *@n: third parameter
  */
-char *_strncpy(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n)
 {
 strncpy(dest, src, n);
 return (dest);
